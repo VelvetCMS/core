7ac1600a2f7d6f535f023bcbe1515ff91a104aea
Michu <michubutanother@proton.me>
2026-02-05 16:17:19 +0100

    chore: release 1.7.0

--- config/version.php
+++ config/version.php
@@ -8,8 +8,8 @@ declare(strict_types=1);
 return [
     /** VelvetCMS Core version information */
     'core' => [
-        'version' => '1.6.0',
-        'release_date' => '2026-02-02',
+        'version' => '1.7.0',
+        'release_date' => '2026-02-05',
         'stability' => 'stable',
         'php' => '^8.4',
         'api_version' => '1',
